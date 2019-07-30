887c25d2e44ed49cd415f7d6a89a49daa3b2b855
sheikhsuleman123 <mssheikh333@gmail.com>
2019-07-30 14:05:46 +0500

    Navbar responsiveness is ready

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from './components/navbar';
+
+
+function App() {
+  return (
+    
+    <div >
+  
+     <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      
+      <nav className="navbar navbar-expand-md navbar-light bg-dark"> 
+        <a href="#" className="navbar-brand">
+        <i className="fa fa-child text-warning fa-2x"> </i></a>
+        <button type="button" className="navbar-toggler bg-light" data-toggle="collapse" data-target="#nav">
+            <span className="navbar-toggler-icon"> </span> </button>
+    <div className="collapse navbar-collapse justify-content-between" id="nav">
+        <ul className="navbar-nav">
+            <li className="nav-item ">
+              <a className="nav-link text-light text-uppercase px-3" href="#">Home</a> </li>
+            <li className="nav-item">
+                <a className="nav-link text-light text-uppercase px-3" href="#">Skills</a> </li>
+            <li className="nav-item dropdown" data-toggle="dropdown">
+                <a className="nav-link text-light 
+                text-uppercase px-3 dropdown-toggle " href="#">Projects</a>
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="">Web</a>
+                    <a className="dropdown-item" href="">Android</a>
+                    <a className="dropdown-item" href="">IOS</a>
+                    <a className="dropdown-item" href="">Hardware</a>
+                </div>
+                 </li>
+            <li className="nav-item">
+                <a className="nav-link text-light text-uppercase px-3" href="#">Team</a> </li>
+            <li className="nav-item">
+                <a className="nav-link text-light text-uppercase px-3" href="#">Contacts</a> </li>
+        </ul>
+
+        <form className="form-inline ml-3">
+            <div className="input-group ">
+                <input type="text" placeholder="Search" className="form-control" />
+                    <div className="input-group-append"> <button type="button" className="btn bg-light">
+                <i className="fa fa-search text-muted"> </i> </button>
+                    </div>
+            </div>
+        </form>
+</div>
+
+      </nav>
+    );
+  }
+}
