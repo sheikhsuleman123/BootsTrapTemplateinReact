28f5ceb663f84da737fa19d3f1842386cff2c296
sheikhsuleman123 <mssheikh333@gmail.com>
2019-08-02 12:54:33 +0500

    Team section is done

--- src/components/team.js
+++ src/components/team.js
@@ -21,6 +21,64 @@ export default class Team extends Component {
                     <div className="col-lg-4">
                         <div className="card">
                             <img src={pic1} className="card-img-top" />
+                            <div className="card-body">
+                                <div className="card-title">
+                                    <h3 className="text-muted">Micheal</h3>
+                                </div>
+                                <div className="card-subtitle">
+                                    <div className="lead text-secondary ">
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, et. Harum quam placeat eveniet ipsam sapiente adipisci cupiditate, voluptatum cum ut. Perspiciatis quos dolorem iste similique repellat. Laudantium, necessi.<b style={{fontWeight: 'bold',}}> "Suleman"</b></p>
+                                    </div>
+                                    <div className="text-right">
+                                        <a href="#"><i className="fa fa-facebook mx-2 fa-2x"></i></a>
+                                        <a href="#"><i className="fa fa-twitter text-info fa-2x"></i></a>
+                                        <a href="#"><i className="fa fa-youtube text-danger mx-2 fa-2x"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="card">
+                            <img src={pic2} className="card-img-top" />
+                            <div className="card-body">
+                                <div className="card-title">
+                                    <h3 className="text-muted">Hamza</h3>
+                                </div>
+                                <div className="card-subtitle">
+                                    <div className="lead text-secondary ">
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, et. Harum quam placeat eveniet ipsam sapiente adipisci cupiditate, voluptatum cum ut. Perspiciatis quos dolorem iste similique repellat. Laudantium necessi.</p>
+                                    </div>
+                                    <div className="text-right">
+                                        <a href="#"><i className="fa fa-facebook mx-2 fa-2x"></i></a>
+                                        <a href="#"><i className="fa fa-twitter text-info fa-2x"></i></a>
+                                        <a href="#"><i className="fa fa-youtube text-danger mx-2 fa-2x"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="card">
+                            <img src={pic3} className="card-img-top" />
+                            <div className="card-body">
+                                <div className="card-title">
+                                    <h3 className="text-muted text-center">Hassan</h3>
+                                </div>
+                                <div className="card-subtitle">
+                                    <div className="lead text-secondary ">
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, et. Harum quam placeat eveniet ipsam sapiente adipisci cupiditate, voluptatum cum ut. Perspiciatis quos dolorem iste similique repellat. Laudantium, necessi.<b style={{fontWeight: 'bold',}}> "Suleman"</b></p>
+                                    </div>
+                                    <div className="text-right">
+                                        <a href="#"><i className="fa fa-facebook mx-2 fa-2x"></i></a>
+                                        <a href="#"><i className="fa fa-twitter text-info fa-2x"></i></a>
+                                        <a href="#"><i className="fa fa-youtube text-danger mx-2 fa-2x"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        
                         </div>
                     </div>
                 </div>
